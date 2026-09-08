2a1c099a664de11616d57c149e5d2f67e8f8690d
Laboratorios Chillan <chilab@inacap.cl>
2026-09-08 09:55:27 -0300

    comas

--- django_templates/urls.py
+++ django_templates/urls.py
@@ -24,7 +24,7 @@ urlpatterns = [
     path('app1/',include('app1.urls')),
     path('appemmanuel/',include('appemmanuel.urls')),
     path('appAndrea/', include('appAndrea.urls')),
-    path('appOmar/', include('appOmar.urls'))
+    path('appOmar/', include('appOmar.urls')),
     path('Benjamín/',include('Benjamín.urls')),
-    path('appAndrea/', include('appAndrea.urls'))
+    path('appAndrea/', include('appAndrea.urls')),
 ]
